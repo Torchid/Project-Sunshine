046dbf2b77c9730c477f19acf6ec513c6352c1c3
Torchid <rachel.l.popa@gmail.com>
2015-10-15 20:39:18 -0600

    feat: On tablets and not on phones the selected day is highlighted in blue

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -64,7 +64,7 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
         Bundle savedInstanceState){
 
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_forecast, container, false);
         foreCastEntriesAdapter = new ForecastAdapter(getActivity(),null, 0);
 
         ListView listOfForecasts = (ListView) rootView.findViewById(R.id.listview_forecast);
